533bd8d6a849701477322446bfe786f30ad72993
Bibeks1008 <bibekshrestha1008@gmail.com>
2025-03-25 23:23:19 +0545

    ADD: [redis cache]

--- project/settings.py
+++ project/settings.py
@@ -94,15 +94,16 @@ if 'test' in sys.argv or 'test_coverage' in sys.argv:
     DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": os.environ.get('REDIS_URL', default='redis://localhost:6379/0'),
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-#         }
-#     }
-# }
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": os.environ.get('REDIS_URL', default='redis://127.0.0.1:6379/1'),
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            'PASSWORD': '123',
+        }
+    }
+}
 
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
--- stock_price_tracking/accessor.py
+++ stock_price_tracking/accessor.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+import json
 import yfinance as yf
 
 class StockAccessor:
@@ -7,7 +9,13 @@ class StockAccessor:
     stock_symbols = ["AAPL","MSFT","AMZN","GOOG","NVDA","META","TSLA","BRK-B","LLY","BRK-A"]
     stock_data = {}
 
-    for symbol in stock_symbols:
+    cached_data = cache.get('stock_data')
+
+    if cached_data:
+      stock_data = json.loads(cached_data)
+      print("data from cache =====> ",stock_data)
+    else:
+      for symbol in stock_symbols:
         try:
             stock = yf.Ticker(symbol)
             
@@ -16,5 +24,7 @@ class StockAccessor:
         except Exception as e:
             print(f"Error fetching data for {symbol}: {e}")
             stock_data[symbol] = None
+
+      cache.set("stock_data", json.dumps(stock_data),900)
 
     return stock_data
